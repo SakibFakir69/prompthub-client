b3ed7bbca3f73064b76cbe25382247d6cb948950
Sakib Fakir <FakirSakib22232@gmail.com>
2026-06-06 20:51:28 +0800

    feat(auth) add all path folder for main

---
+++ src/app/(main)/explore/page.tsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        this is explore page
+
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/(main)/saved/page.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+function page() {
+  return (
+    <div>saved</div>
+  )
+}
+
+export default page
