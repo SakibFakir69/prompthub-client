540d459f21dae422ce900c641d53d21d91643827
Sakib Fakir <FakirSakib22232@gmail.com>
2026-07-28 01:18:01 +0800

    feat(metadata) add static meta data

--- src/app/prompt-details/[id]/page.tsx
+++ src/app/prompt-details/[id]/page.tsx
@@ -1,20 +1,17 @@
+import type { Metadata } from 'next'
+import { cookies } from 'next/headers';
 import PromptDetails from "@/src/components/prompt/prompt-details";
-
-
 
 interface PageProps {
   params: Promise<{ id: string }>;
 }
 
-async function PromptDetailsPage({ params }:PageProps) {
 
 
+async function PromptDetailsPage({ params }: PageProps) {
   const { id } = await params;
-  console.log(id)
 
-
-
-  return <PromptDetails id={id}/>
+  return <PromptDetails id={id} />;
 }
 
 export default PromptDetailsPage;
