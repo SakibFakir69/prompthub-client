866ae57abd37db14bf133273dea8fef54791b303
Sakib Fakir <FakirSakib22232@gmail.com>
2026-07-28 20:38:11 +0800

    feat(logo) add logo on left side prompt

--- src/components/main/LeftSidebar.tsx
+++ src/components/main/LeftSidebar.tsx
@@ -1,6 +1,8 @@
 import { cookies } from 'next/headers';
 import { MiniProfile } from './profile/profile';
 import { NavLinks } from './left-sidebar-menu';
+import Image from 'next/image';
+import Link from 'next/link';
 
 
 export const getProfileInfo = async () => {
@@ -39,9 +41,10 @@ export default async function LeftSidebar() {
     
     
       <div className="px-3 py-2 mb-5">
-        <span className="text-lg font-semibold tracking-tight">
-          Prompt<span className="text-[#FF6B35]">Hub</span>
-        </span>
+        <Link prefetch={true} href={'/home'}>
+        <Image src={'/ph-logo-2-bg.png'} alt={'PromptHub'} height={180} width={150}/>
+        </Link>
+
       </div>
 
     
