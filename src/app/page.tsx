2d28bdc0a437f0ad6f5e73b4695ada0fb3d348b1
Sakib Fakir <FakirSakib22232@gmail.com>
2026-08-02 22:18:27 +0800

    remove:marketplace component and prompt-card compoenent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Footer from "../components/landing-page/footer";
 import Banner from "../components/landing-page/banner";
 import Pricing from "../components/landing-page/pricing";
 import Features from "../components/landing-page/features";
-import { MarketPlace } from "../components/landing-page/market-place";
 import HowItWorks from "../components/landing-page/how-it-work";
 import Navbar from "../components/landing-page/nav-bar";
 import Hero from "../components/landing-page/hero";
